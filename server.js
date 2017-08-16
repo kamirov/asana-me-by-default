101fce0e25c5104e3d05eee8d1239f7d74516bad
andrei <andrei.khramtsov@gmail.com>
2017-08-15 22:50:20 -0400

    Add stub routes

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+
+
+// Initialization
+app.post('/init', function (req, res) {
+
+    // Get list of workspaces
+
+    // Get list of projects in each workspace
+    
+    // Register webhook for each project
+
+});
+
+
+// Webhook handler
+app.post('/update/:projectId', function(req, res) {
+    
+});
+
+
+app.listen(3000, function () {});
