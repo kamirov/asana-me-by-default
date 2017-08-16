74c65e5ca4ff599f8e85ba382c633b95bcd97f86
andrei <andrei.khramtsov@gmail.com>
2017-08-16 00:21:27 -0400

    GET workspaces, add some general request code

--- server.js
+++ server.js
@@ -1,13 +1,32 @@
+'use strict';
+
 const request = require('request');
+const rp = require('request-promise')
 const express = require('express');
 const config = require('./config');
 const app = express();
 
+let requestOptions = {
+    headers: {
+        'Authorization': 'Bearer ' + config.PERSONAL_ACCESS_TOKEN
+    }, 
+    json: true     
+};
+
+const asanaApiUrl = 'https://app.asana.com/api/1.0';
+let urls = {
+    allWorkSpaces: asanaApiUrl + '/workspaces'
+};
 
 // Initialization
 app.post('/init', (req, res) => {
 
     // Get list of workspaces
+    rp.get(urls.allWorkSpaces, requestOptions)
+        .then((response) => response.data)
+        .then((workspaces) => {
+            
+        });
 
     // Get list of projects in each workspace
     
