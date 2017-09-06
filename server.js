81c0436b2ccc8592c41d9adde0b6a3efee72aea7
andrei <andrei@sampler.io>
2017-09-06 00:11:09 -0400

    Clean up routes, add helpers

--- server.js
+++ server.js
@@ -24,93 +24,42 @@ let urls = {
     updateTask: asanaApiUrl + '/tasks/:taskId'
 };
 
-
 // Middleware
 app.use( bodyParser.json() );
+app.listen(config.PORT, () => console.log('Listening') );
 
-app.get('/', (req, res) => {
-    res.send('Here');
-    console.log('request');    
-})
+
+// ===========
+// Controllers
+// ===========
 
 // Initialization
 app.post('/init', (req, res) => {
-    
-    // Get list of workspaces
     rp.get(urls.allWorkSpaces, requestOptions)
         .then((response) => response.data)
         .then((workspaces) => {
-
-            // Get list of projects in each workspace
             workspaces.forEach((workspace) => {
-                let allProjectsRequestOptions = Object.assign({}, requestOptions, {
-                    qs: {
-                        workspace: workspace.id
-                    }
-                });
-
-                rp.get(urls.allProjects, allProjectsRequestOptions)
-                    .then((response) => response.data)
-                    .then((projects) => {
-                        projects.forEach((project) => {
-                            let webhookRequestOptions = Object.assign({}, requestOptions, {
-                                body: {
-                                    data: {
-                                        resource: project.id,
-                                        target: config.BASE + '/update/' + project.id                        
-                                    }
-                                }
-                            });
-
-                            // Register webhook for each project
-                            rp.post(urls.receiveWebhook, webhookRequestOptions);
-                        });
-                        
-                    })
+                getWorkspaceProjects(workspace)
+                    .then(getResponseData)
+                    .then((projects) => projects.forEach((project) => initProjectWebhook(project)));
             });
-
-            // res.json(workspaces);            
         });
         
     res.send();
 });
 
 
-// Webhook handler 
+// Webhook handler
 app.post('/update/:projectId', (req, res) => {    
-    // TODO: Cache and check this
-    let xHookSecret = req.get('X-Hook-Secret');
+    performHandshake(req, res);
     
-    if (xHookSecret) {
-        res.set({
-            'X-Hook-Secret': xHookSecret
-        });
-        console.log('xHook', xHookSecret);
-    }
-
     if (req.body && req.body.events) {
         let addTaskEvents = req.body.events.filter((event) => {
             return event.type === 'task' && event.action == 'added'
         });
 
-        if (addTaskEvents) {
-            console.log(addTaskEvents);
-            addTaskEvents.forEach((addTaskEvent) => {
-                let updateTaskUrl = urls.updateTask
-                    .replace(':taskId', addTaskEvent.resource);
-                    
-                let updateTaskRequestOptions = Object.assign({}, requestOptions, {
-                    body: {
-                        data: {
-                            assignee: 'me'                        
-                        }
-                    }
-                });
-
-                console.log(updateTaskUrl, updateTaskRequestOptions)
-    
-                rp.put(updateTaskUrl, updateTaskRequestOptions);
-            });
+        if (addTaskEvents.length) {
+            addTaskEvents.forEach((addTaskEvent) => assignTaskToMe(addTaskEvent.resource));
         }
     }
 
@@ -120,67 +69,120 @@ app.post('/update/:projectId', (req, res) => {
 
 // GET all webhooks
 app.get('/webhooks', (req, res) => {
-    console.log(req.params);
-
-    // Get all workspaces
     rp.get(urls.allWorkSpaces, requestOptions)
-        .then((response) => response.data)
-        .then((workspaces) => {
-        console.log(workspaces);
-
-        // Get all webhooks in each workspace
-            workspaces.forEach((workspace) => {
-                let webhookRequestOptions = Object.assign({}, requestOptions, {
-                    qs: {
-                        workspace: workspace.id,
-                    }
-                });
-
-                rp.get(urls.getWorkspaceWebhooks, webhookRequestOptions)
-                    .then((response) => {
-                        console.log(workspace.name, response.data);
-                    });
-            });
-        });
+        .then(getResponseData)
+        .then((workspaces) => workspaces.forEach((workspace) => getWorkspaceWebhooks(workspace)));
         
     res.send();
 });
 
+
 // DELETE all webhooks
-app.delete('/webhooks', (req, res) => {
-    console.log(req.params);
-    
-        // Get all workspaces
-        rp.get(urls.allWorkSpaces, requestOptions)
-            .then((response) => response.data)
-            .then((workspaces) => {
-            console.log(workspaces);
-    
-            // Get all webhooks in each workspace
-                workspaces.forEach((workspace) => {
-                    let webhookRequestOptions = Object.assign({}, requestOptions, {
-                        qs: {
-                            workspace: workspace.id,
-                        }
-                    });
-    
-                    rp.get(urls.getWorkspaceWebhooks, webhookRequestOptions)
-                        .then((response) => response.data)
-                        .then((webhooks) => {
-                            console.log(workspace.name, webhooks.data);
-
-                            // Delete each webhook
-                            webhooks.forEach((webhook) => {
-                                let deleteWebhookUrl = urls.deleteWebhook
-                                    .replace(':webhookId', webhook.id)
-
-                                rp.delete(deleteWebhookUrl, requestOptions);
-                            });
-                        });
-                });
-            });
-
+app.delete('/webhooks', (req, res) => {    
+    rp.get(urls.allWorkSpaces, requestOptions)
+        .then(getResponseData)
+        .then((workspaces) => workspaces.forEach((workspace) => deleteWorkspaceWebhooks(workspace)));
     res.send();
 });
 
-app.listen(config.PORT, () => console.log('Listening') );
+
+// =======
+// Helpers
+// =======
+
+function getResponseData(response) {
+    return response.data;
+}
+
+
+function performHandshake(req, res) {
+    let xHookSecret = req.get('X-Hook-Secret');
+    if (xHookSecret) {
+        console.log('Shaking hands');
+        res.set({
+            'X-Hook-Secret': xHookSecret
+        });
+    }
+}
+
+
+function initProjectWebhook(project) {
+    console.log('Initiating webhook for project ' + project.name);    
+    let webhookRequestOptions = Object.assign({}, requestOptions, {
+        body: {
+            data: {
+                resource: project.id,
+                target: config.BASE + '/update/' + project.id                        
+            }
+        }
+    });
+
+    rp.post(urls.receiveWebhook, webhookRequestOptions);
+}
+
+
+function getWorkspaceProjects(workspace) {
+    let allProjectsRequestOptions = Object.assign({}, requestOptions, {
+        qs: {
+            workspace: workspace.id
+        }
+    });
+
+    return rp.get(urls.allProjects, allProjectsRequestOptions)
+}
+
+
+function getWorkspaceWebhooks(workspace) {
+    let webhookRequestOptions = Object.assign({}, requestOptions, {
+        qs: {
+            workspace: workspace.id,
+        }
+    });
+
+    return rp.get(urls.getWorkspaceWebhooks, webhookRequestOptions)
+        .then((response) => {
+            console.log(workspace.name, response.data);
+        });
+}
+
+
+function deleteWorkspaceWebhooks(workspace) {
+    console.log('Deleting webhooks from workspace ' + workspace.name);
+    let webhookRequestOptions = Object.assign({}, requestOptions, {
+        qs: {
+            workspace: workspace.id,
+        }
+    });
+
+    return rp.get(urls.getWorkspaceWebhooks, webhookRequestOptions)
+        .then((response) => response.data)
+        .then((webhooks) => deleteWebhooks(webhooks));
+}
+
+
+function deleteWebhooks(webhooks) {
+    webhooks.forEach((webhook) => {
+        let deleteWebhookUrl = urls.deleteWebhook
+            .replace(':webhookId', webhook.id)
+
+        rp.delete(deleteWebhookUrl, requestOptions);
+    });
+}
+
+
+function assignTaskToMe(taskId) {
+    console.log('Self-assigning task with ID: ' + taskId);
+
+    let updateTaskUrl = urls.updateTask
+        .replace(':taskId', taskId);
+    
+    let updateTaskRequestOptions = Object.assign({}, requestOptions, {
+        body: {
+            data: {
+                assignee: 'me'                        
+            }
+        }
+    });
+
+    return rp.put(updateTaskUrl, updateTaskRequestOptions);
+}
