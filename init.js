2d5187a7b42a27fc589ed4e8decb485bb5a06a70
andrei <andrei@sampler.io>
2017-09-07 22:48:45 -0400

    Handle tunnel creation error

--- init.js
+++ init.js
@@ -5,7 +5,12 @@ const ngrok = require('ngrok');
 const server = require('./server');
 
 ngrok.connect(config.PORT, (err, url) => {
-    console.log(url);
+    if (err) {
+        console.error('Couldn\'t establish https tunnel. Is the port ' + config.PORT + ' available?');    
+        process.exit();    
+    }
+
+    console.log('Established tunnel to localhost:' + config.PORT + ' through URL: ' + url);
     server.setBase(url);
     server.init();
 });
